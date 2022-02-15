9b6208674b0403d22ff19247c5d642085fa5359f
Vinicius <vncsna@gmail.com>
2022-02-15 17:05:36 -0300

    feat: quinto capítulo

---
+++ 5.1-two-penny/README.md
@@ -0,0 +1,1 @@
+Algumas regiões estão com recursos limitados, logo altere `region` em `terraform.tfvars`
---
+++ 5.1-two-penny/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type    = string
+  default = "West US"
+}
+
+variable "namespace" {
+  type    = string
+  default = "ballroominaction"
+}
